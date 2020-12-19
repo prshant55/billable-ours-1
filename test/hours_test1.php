9a070db8789e5282d99bd8e3d39a2516280b0c41
prshant55 <72465599+prshant55@users.noreply.github.com>
2020-12-19 13:16:41 -0800

    Create hours_test1.php

---
+++ test/hours_test1.php
@@ -0,0 +1,20 @@
+<?php
+//hours_test1.php
+include_once 'includes/settings.php';
+require_once 'simpletest/autorun.php';
+require_once 'simpletest/web_tester.php';
+
+class hoursForm extends WebTestCase {
+
+	  function testCorrectData() {
+		$this->get(VIRTUAL_PATH . '/hours.php');
+		$this->assertResponse(200);
+
+        $this->setField("hours", "5");
+		$this->setField("rate", "10");
+		$this->clickSubmit("Show Pay");
+
+		$this->assertResponse(200);
+		$this->assertText("You input 5 hours at a rate of $10 and your pay is $50");
+	}
+	}
