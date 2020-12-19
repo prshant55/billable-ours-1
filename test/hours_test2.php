e48dffe370b5c21b91aae4894d60fa8aabdefc8b
prshant55 <72465599+prshant55@users.noreply.github.com>
2020-12-19 13:26:18 -0800

    Update hours_test2.php

--- test/hours_test2.php
+++ test/hours_test2.php
@@ -7,7 +7,7 @@ require_once 'simpletest/web_tester.php';
 class hoursForm extends WebTestCase {
 
 	  function testCorrectData() {
-		$this->get(VIRTUAL_PATH . '/hours.php');
+		$this->get(VIRTUAL_PATH . '/hours2.php');
 		$this->assertResponse(200);
 
         $this->setField("hours", "100");
